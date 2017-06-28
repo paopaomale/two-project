1c2e5475c44fa41f0d8f4684a609136f6a25f036
paopao <paopapmale@gmail.com>
2017-06-29 00:04:56 +0800

    配置 package

---
+++ script.js
@@ -0,0 +1,7 @@
+var date = new Date();
+var hour = date.getHours();
+var minute = date.getMinutes();
+var second = date.getSeconds();
+var mill = date.getMilliseconds(); 
+
+console.log(hour+'时'+minute+'分'+second+'秒'+mill);
